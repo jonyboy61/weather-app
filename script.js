fd3249b85e554565be8b6b013923c9550ae9ee9f
Joao Souto <92180579+jonyboy61@users.noreply.github.com>
2025-08-20 18:32:41 +0200

    Update script.js
    Update path for search

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 document.getElementById("search-btn").addEventListener("click", () => {
   const city = document.getElementById("city-input").value.trim();
   const encodedCity = encodeURIComponent(city);
-  window.location.href = `./forecast/forecast.html?city=${encodedCity}`;
+  window.location.href = "https://jonyboy61.github.io/weather-app/forecast/forecast.html?city=" + encodeURIComponent(city);
+
 
 });
 
